27a221058195c0efe1e56f5d7e19dafe4815f35e
Sreenivasulu Gadige <sreenivasulu.gadige@tesco.com>
2018-05-13 20:28:29 +0530

    Hello World program in for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,9 @@ public class HelloWorld {
 
 	  public static void main(String[] args) {
 
-	        System.out.println("Hello World");
+		  for(int i=0;i<5; i++) {
+	        	 System.out.println(i +" ...Hello World");
+	        }
 	    }
 	
 }
